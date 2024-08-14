b2dc6957e9cbd413317c3f55719e40a7264bd9dc
bruno_senigalha <brsenigalha@gmail.com>
2024-08-14 17:45:28 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/brunoSenigalha/course/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.brunoSenigalha.course.repositories;
+
+import com.brunoSenigalha.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
