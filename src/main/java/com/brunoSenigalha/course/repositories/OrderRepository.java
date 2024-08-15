5e1de6d76edb678db883117b46e763b2aed37b9b
Bruno Senigalha <brsenigalha@gmail.com>
2024-08-15 14:38:32 -0300

    Order, Instant, ISO 8601 part 3

---
+++ src/main/java/com/brunoSenigalha/course/repositories/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.brunoSenigalha.course.repositories;
+
+import com.brunoSenigalha.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ src/main/java/com/brunoSenigalha/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.brunoSenigalha.course.services;
+
+import com.brunoSenigalha.course.entities.Order;
+import com.brunoSenigalha.course.entities.User;
+import com.brunoSenigalha.course.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll(){
+        return repository.findAll();
+    }
+
+    public Order findById(Long id){
+        Optional<Order> obj = repository.findById(id);
+        return obj.get();
+    }
+}
