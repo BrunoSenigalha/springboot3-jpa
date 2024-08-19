8619c154c77bee58c90099bac5379c2ce7b4a652
Bruno Senigalha <brsenigalha@gmail.com>
2024-08-19 14:00:18 -0300

    OrderItem, many-to-many association with extra attributes Part 2

--- src/main/java/com/brunoSenigalha/course/repositories/OrderItemRepository.java
+++ src/main/java/com/brunoSenigalha/course/repositories/OrderItemRepository.java
@@ -1,9 +1,9 @@
 package com.brunoSenigalha.course.repositories;
 
-import com.brunoSenigalha.course.entities.Order;
+import com.brunoSenigalha.course.entities.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
--- src/main/java/com/brunoSenigalha/course/repositories/UserRepository.java
+++ src/main/java/com/brunoSenigalha/course/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.brunoSenigalha.course.repositories;
 
 import com.brunoSenigalha.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
