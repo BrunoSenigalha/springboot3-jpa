2a3f43ee5e983a158af74b847208a7438fc9fc85
Bruno Senigalha <brsenigalha@gmail.com>
2024-08-27 14:35:35 -0300

    ExceptionHandler

--- src/main/java/com/brunoSenigalha/course/services/UserService.java
+++ src/main/java/com/brunoSenigalha/course/services/UserService.java
@@ -2,9 +2,13 @@ package com.brunoSenigalha.course.services;
 
 import com.brunoSenigalha.course.entities.User;
 import com.brunoSenigalha.course.repositories.UserRepository;
+import com.brunoSenigalha.course.services.exceptions.DatabaseException;
 import com.brunoSenigalha.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,7 +33,14 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        User entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(id));
+        try{
+            repository.delete(entity);
+
+        }catch(DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
